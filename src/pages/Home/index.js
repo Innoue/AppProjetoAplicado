b92e8fcff42afb66558c0ad71a83e625db3cda96
Inoue <rodrigo.inoue1122@gmail.com>
2022-12-15 14:02:07 -0300

    removed navigation in home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,14 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
-
-import { useNavigation } from '@react-navigation/native'
+import { View, StyleSheet } from 'react-native'
 
 import { CardRecipe } from '../../components/CardRecipe';
 
 export default function Home(){
-  const navigation = useNavigation();
-
-  function navigate(){
-    navigation.navigate('Recipe')
-  }
-
   return(
     <View style={styles.container}>
       <View style={{height:150,width:'90%'}}>
         <CardRecipe title={'Pudim'} type={'Sobremesa'} country={'Brasil'} doTime={50}/>
       </View>
-      <Text>Home</Text>
-      <Button title='Recipe' onPress={navigate}/>
     </View>
   )
 }
